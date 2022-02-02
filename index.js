e6932b55317355ddfca0d3b114b15fe8a17b341d
Michael Kenyi <michaelkenyi13@gmail.com>
2022-02-02 09:49:16 +0600

    update

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-const tabBtn = document.getElementById("saveTab-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    console.log(myLeads);
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = "";
@@ -25,23 +34,15 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
-});
-
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
   render(myLeads);
 });
 
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, activeWindow: true }, function () {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
